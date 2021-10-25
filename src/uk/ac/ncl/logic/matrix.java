3e9285cc7707031ae69f3fe6b031ee95d0d76b69
Fiona Lapp <fiona.lapp.fg@gmail.com>
2021-10-25 09:46:32 +0100

    Initialising the project

---
+++ src/uk/ac/ncl/logic/matrix.java
@@ -0,0 +1,22 @@
+package uk.ac.ncl.logic;
+
+public class matrix {
+	
+	public matrix() {
+		//I'm just gonna hardcode in a matrix for now
+		int[][] adjacencyMatrix= {{0, 1, 1, 0}, 
+								{1, 0, 1, 0},
+								{1, 1, 0, 0},
+								{0,0,0,0} 
+											};//each 1 represents that there is a connection, 0 that there isn't
+		
+		
+		boolean [][] received= new boolean[4][4];// each row contains the rec[] vector for that node
+		
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+
+}

27268ea39938a0724e9d48c89ecdf29d43d0daa9
Fiona Lapp <fiona.lapp.fg@gmail.com>
2021-11-25 14:23:08 +0000

    Added documentation to message interface

--- src/uk/ac/ncl/logic/Message.java
+++ src/uk/ac/ncl/logic/Message.java
@@ -1,10 +1,33 @@
 package uk.ac.ncl.logic;
 
+/**
+ * An interface for messages. They have a sender id, receiver id, and a field for the remaining time of transit.
+ * @author Fiona Lapp
+ *
+ */
 public interface Message extends Comparable<Message> {
+	/**
+	 * id of the node that sent the message
+	 * @return the sender id
+	 */
 	public Node getSender();
+	/**
+	 * id of the node that will receive the message
+	 * @return the receiver id
+	 */
 	public Node getRecipient();
+	/**
+	 * The transit time that is left for this message
+	 * @return how long the message will still be in transit
+	 */
 	public int getTimeLeft();
+	
 	public int compareTo(Message message);
+	/**
+	 * decrease the remaining transit time by 1. 
+	 * <p>This method should be called on every message at every time step of the algorithm.</p>
+	 * <p>If the time left is already 0, nothing will happen</p>
+	 */
 	public void decreaseTimeLeft();// by one
 	
 }

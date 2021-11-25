179d9dba58fb450a281a1226e49349a6077c7c70
Fiona Lapp <fiona.lapp.fg@gmail.com>
2021-11-25 15:53:40 +0000

    Add check for symmetry in adjacency matrix

--- src/uk/ac/ncl/logic/ImplementedGraph.java
+++ src/uk/ac/ncl/logic/ImplementedGraph.java
@@ -5,6 +5,14 @@ public class ImplementedGraph {
 	private int[][] adjacencyMatrix;
 	private int size;
 	private Node[] nodes;
+	/**
+	 * Create a graph 
+	 * <p>a matrix specifying how long it takes messages to be sent between nodes. Rows represent the sender, columns the receiver.
+	 * If a value is set to Integer.MAX_VALUE, this is interprested as no connection being present.
+	 * 
+	 * </p>
+	 * @param connectionMatrix a matrix specifying how long it takes messages to be sent between nodes. Rows represent the sender, columns the receiver.
+	 */
 	public ImplementedGraph(int[][] connectionMatrix) {
 		if (connectionMatrix[0].length!=connectionMatrix.length) {
 			throw new IllegalArgumentException("Needs to be a square matrix");
@@ -20,15 +28,36 @@ public class ImplementedGraph {
 	}
 	
 
+	/**
+	 * creates an adjacency matrix using the connection matrix field. The adjacency matrix is a symmetrical matrix with each cell being 1 if there is a connection, 0 if there isn't.
+	 */
 	private void createAdjacencyMatrix(){
 		adjacencyMatrix=new int[size][size];
 		for (int row=0; row<size; row++) {
-			for (int col=0; col<size; col++) {
+			for (int col=0; col<=row; col++) {
 				int connectionLength=connectionMatrix[row][col];
-			adjacencyMatrix[row][col]=(connectionLength==Integer.MAX_VALUE)? 0:1; //set connection to 0(false) if connection length is infinity, else seit it to true (1)
-			
+				int oppositeLength= connectionMatrix[col][row];
+				int m= Integer.MAX_VALUE;
+				if (connectionLength==m) {
+					if (oppositeLength==m){
+			adjacencyMatrix[row][col]=0; //set connection to 0(false) if connection length is infinity
+			adjacencyMatrix[col][row]=0;
+					}
+					else{
+						throw new IllegalArgumentException("graph needs to have symmetrical connections");
+					}}
+				else {
+					if (!( oppositeLength==m)){
+						adjacencyMatrix[row][col]=1; //set connection to 1(true) if connection length is less than infinity
+						adjacencyMatrix[col][row]=1;
+					}
+					else{
+						throw new IllegalArgumentException("graph needs to have symmetrical connections");
+					}
+				}
 			}
 			}
+		
 		}
 	
 	

133bfa60e8fb26e5846e9eed0694a7b2c64215de
Fiona Lapp <fiona.lapp.fg@gmail.com>
2021-11-25 13:24:15 +0000

    implemented message class

---
+++ src/uk/ac/ncl/logic/ImplementedMessage.java
@@ -0,0 +1,69 @@
+package uk.ac.ncl.logic;
+
+import java.util.Objects;
+
+public class ImplementedMessage implements Message {
+	
+	private final Node sender;
+	private final Node receiver;
+	private int timeLeft;
+	
+	public ImplementedMessage(Node sender,Node receiver, int timeLeft) {
+		this.sender=sender;
+		this.receiver= receiver;
+		this.timeLeft=timeLeft;
+	}
+
+	@Override
+	public Node getSender() {
+		return sender;
+	}
+
+
+
+	@Override
+	public Node getRecipient() {
+		return receiver;
+	}
+
+	@Override
+	public int getTimeLeft() {
+		return timeLeft;
+	}
+
+	/**
+	 * This returns a positive number if this message has more time left than the message it is being compared to.
+	 */
+	@Override
+	public int compareTo(Message message) {
+		return this.timeLeft-message.getTimeLeft();
+	}
+	@Override
+	public void decreaseTimeLeft() { //by one
+		if (timeLeft>0)
+		{timeLeft--;}
+		// if the message has already arrived in the buffer, nothing happens
+	
+	}
+	@Override
+	public String toString() {
+		return "ImplementedMessage from " + sender + " to " + receiver + " has " + timeLeft + " time units left until arrival";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(receiver, sender, timeLeft);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof ImplementedMessage))
+			return false;
+		ImplementedMessage other = (ImplementedMessage) obj;
+		return Objects.equals(receiver, other.receiver) && Objects.equals(sender, other.sender)
+				&& timeLeft == other.timeLeft;
+	}
+
+}

fcbf6f1eb42acd7c99994fe4087bdec3e0c7270f
Fiona Lapp <fiona.lapp.fg@gmail.com>
2021-11-25 15:56:59 +0000

    added documentation for implemnted graph

--- src/uk/ac/ncl/logic/ImplementedGraph.java
+++ src/uk/ac/ncl/logic/ImplementedGraph.java
@@ -62,7 +62,8 @@ public class ImplementedGraph {
 	
 	
 	/**
-	 * @return the nodes
+	 * nodes in the graph
+	 * @return the nodes an array containing all node objects of the graph
 	 */
 	public Node[] getNodes() {
 		return nodes;
@@ -70,7 +71,8 @@ public class ImplementedGraph {
 
 
 	/**
-	 * @return the size
+	 * size of the graph
+	 * @return the size the size of the graph
 	 */
 	public int getSize() {
 		return size;
@@ -78,6 +80,8 @@ public class ImplementedGraph {
 
 
 	/**
+	 * connection matrix 
+	 * <p>a matrix containing the length of each connection (rows represent senders, columns recipients)</p>
 	 * @return the connectionMatrix
 	 */
 	public int[][] getConnectionMatrix() {
@@ -85,6 +89,8 @@ public class ImplementedGraph {
 	}
 
 	/**
+	 * adjacency matrix
+	 * <p>each cell contains a 0 if there is no connection, a 1 if there is a connection (by default symmetrical). Rows represent senders, columns recipients.</p>
 	 * @return the adjacencyMatrix
 	 */
 	public int[][] getAdjacencyMatrix() {
